1d6d5250825c67e97da02b543538ac435adbbf95
DaCodeChick <peepvtuber@gmail.com>
2025-04-05 14:48:42 -0700

    Let's not overcomplicate this

--- haxial/src/net.rs
+++ haxial/src/net.rs
@@ -11,9 +11,9 @@ pub struct Session {
 
 impl Session {
     pub fn new(connection: Arc<Mutex<Connection>>) -> Arc<Mutex<Session>> {
-        let time = Local::now().timestamp();
-        let hi = time.wrapping_shr(32) & (u32::MAX as i64);
-        let lo = time & (u32::MAX as i64);
+        let time = Local::now().timestamp() as u64;
+        let hi = time.wrapping_shr(32) & (u32::MAX as u64);
+        let lo = time & (u32::MAX as u64);
 
         Arc::new(Mutex::new(Session {
             id: shuffle64(time, hi).wrapping_shl(32) | (shuffle64(time, lo) & (u32::MAX as u64)),
@@ -22,9 +22,9 @@ impl Session {
     }
 }
 
-const fn shuffle64(value: i64, part: i64) -> u64 {
-    (value & 0xFF000000FF00i64.wrapping_shr(32).wrapping_shl(8)
+const fn shuffle64(value: u64, part: u64) -> u64 {
+    value & 0xFF000000FF00u64.wrapping_shr(32).wrapping_shl(8)
         | part.wrapping_shr(24)
         | part.wrapping_shr(8) & 0xFF00
-        | part.wrapping_shl(24)) as u64
+        | part.wrapping_shl(24)
 }

a1eb9e5d2989712420854bc6cdb45add6ee9386d
DaCodeChick <peepvtuber@gmail.com>
2025-05-01 14:33:46 -0700

    Separate MD5, implement Twofish

--- haxial/src/algo.rs
+++ haxial/src/algo.rs
@@ -1,5 +1,7 @@
 use bytemuck::{cast_slice, cast_slice_mut};
 
+use crate::{CryptError, Md5};
+
 const CRCTAB: [u32; 256] = [
     0, 0x4C11DB7, 0x9823B6E, 0xD4326D9, 0x130476DC, 0x17C56B6B, 0x1A864DB2, 0x1E475005, 0x2608EDB8,
     0x22C9F00F, 0x2F8AD6D6, 0x2B4BCB61, 0x350C9B64, 0x31CD86D3, 0x3C8EA00A, 0x384FBDBD, 0x4C11DB70,
@@ -35,269 +37,10 @@ const CRCTAB: [u32; 256] = [
     0xAB710D06, 0xA6322BDF, 0xA2F33668, 0xBCB4666D, 0xB8757BDA, 0xB5365D03, 0xB1F740B4,
 ];
 
-/// A simple MD5 implementation
-#[derive(Debug)]
-pub struct Md5 {
-    state: [u32; 4],
-    count: [u32; 2],
-    buf: [u8; 64],
-    buf_len: usize,
-}
-
-impl Md5 {
-    /// Creates a new MD5 instance
-    pub fn new() -> Self {
-        Self {
-            state: [0x67452301, 0xEFCDAB89, 0x98BADCFE, 0x10325476],
-            count: [0xC3D2E1F0, 0],
-            buf: [0; 64],
-            buf_len: 0,
-        }
-    }
-
-    /// Updates the MD5 state with the given input data
-    pub fn update(&mut self, input: Option<&[u8]>) {
-        if self.buf_len == 64 {
-            self.transform(None);
-            self.buf_len = 0;
-            self.count[1] = self.count[1].wrapping_add(1);
-        }
-
-        if let Some(mut data) = input {
-            if self.buf_len > 0 {
-                while !data.is_empty() && self.buf_len < 64 {
-                    self.buf[self.buf_len] = data[0];
-                    self.buf_len += 1;
-                    data = &data[1..];
-                }
-
-                if self.buf_len == 64 {
-                    self.transform(None);
-                    self.buf_len = 0;
-                    self.count[1] = self.count[1].wrapping_add(1);
-                }
-
-                if data.is_empty() {
-                    return;
-                }
-            }
-
-            while data.len() >= 64 {
-                self.transform(Some(&data[..64]));
-                data = &data[64..];
-                self.count[1] = self.count[1].wrapping_add(1);
-            }
-
-            data.iter().for_each(|&byte| {
-                self.buf[self.buf_len] = byte;
-                self.buf_len += 1;
-            });
-        }
-    }
-
-    /// Returns the MD5 hash as a 16-byte array
-    pub fn report(&mut self) -> [u8; 16] {
-        let mut digest = [0u8; 16];
-        self.update(None);
-
-        let bit_count: u64 = ((self.count[1] as u64) << 29)
-            + ((self.count[0] as u64) << 3)
-            + ((self.buf_len as u64) << 3);
-
-        self.buf[self.buf_len] = 0x80;
-        self.buf_len += 1;
-
-        if self.buf_len > 56 {
-            while self.buf_len < 64 {
-                self.buf[self.buf_len] = 0;
-                self.buf_len += 1;
-            }
-            self.transform(None);
-            self.buf_len = 0;
-        }
-
-        self.buf[self.buf_len..56].fill(0);
-        self.buf[56..64]
-            .iter_mut()
-            .enumerate()
-            .for_each(|(i, b)| *b = (bit_count >> (i << 3)) as u8);
-
-        self.transform(None);
-
-        let digest32: &mut [u32] = cast_slice_mut(&mut digest[..]);
-        self.state
-            .iter()
-            .enumerate()
-            .for_each(|(i, &chunk)| digest32[i] = chunk.to_be());
-
-        self.state.fill(0);
-        self.count.fill(0);
-        self.buf.fill(0);
-
-        digest
-    }
-
-    fn transform(&mut self, block: Option<&[u8]>) {
-        let mut a = self.state[0];
-        let mut b = self.state[1];
-        let mut c = self.state[2];
-        let mut d = self.state[3];
-
-        let block: &[u32] = if let Some(block) = block {
-            cast_slice(block)
-        } else {
-            cast_slice(&self.buf[..])
-        };
-
-        a = ff(a, b, c, d, block[0], 7, 0xD76AA478);
-        d = ff(d, a, b, c, block[1], 12, 0xE8C7B756);
-        c = ff(c, d, a, b, block[2], 17, 0x242070DB);
-        b = ff(b, c, d, a, block[3], 22, 0xC1BDCEEE);
-        a = ff(a, b, c, d, block[4], 7, 0xF57C0FAF);
-        d = ff(d, a, b, c, block[5], 12, 0x4787C62A);
-        c = ff(c, d, a, b, block[6], 17, 0xA8304613);
-        b = ff(b, c, d, a, block[7], 22, 0xFD469501);
-        a = ff(a, b, c, d, block[8], 7, 0x698098D8);
-        d = ff(d, a, b, c, block[9], 12, 0x8B44F7AF);
-        c = ff(c, d, a, b, block[10], 17, 0xFFFF5BB1);
-        b = ff(b, c, d, a, block[11], 22, 0x895CD7BE);
-        a = ff(a, b, c, d, block[12], 7, 0x6B901122);
-        d = ff(d, a, b, c, block[13], 12, 0xFD987193);
-        c = ff(c, d, a, b, block[14], 17, 0xA679438E);
-        b = ff(b, c, d, a, block[15], 22, 0x49B40821);
-
-        a = gg(a, b, c, d, block[1], 5, 0xF61E2562);
-        d = gg(d, a, b, c, block[6], 9, 0xC040B340);
-        c = gg(c, d, a, b, block[11], 14, 0x265E5A51);
-        b = gg(b, c, d, a, block[0], 20, 0xE9B6C7AA);
-        a = gg(a, b, c, d, block[5], 5, 0xD62F105D);
-        d = gg(d, a, b, c, block[10], 9, 0x02441453);
-        c = gg(c, d, a, b, block[15], 14, 0xD8A1E681);
-        b = gg(b, c, d, a, block[4], 20, 0xE7D3FBC8);
-        a = gg(a, b, c, d, block[9], 5, 0x21E1CDE6);
-        d = gg(d, a, b, c, block[14], 9, 0xC33707D6);
-        c = gg(c, d, a, b, block[3], 14, 0xF4D50D87);
-        b = gg(b, c, d, a, block[8], 20, 0x455A14ED);
-        a = gg(a, b, c, d, block[13], 5, 0xA9E3E905);
-        d = gg(d, a, b, c, block[2], 9, 0xFCEFA3F8);
-        c = gg(c, d, a, b, block[7], 14, 0x676F02D9);
-        b = gg(b, c, d, a, block[12], 20, 0x8D2A4C8A);
-
-        a = hh(a, b, c, d, block[5], 4, 0xFFFA3942);
-        d = hh(d, a, b, c, block[8], 11, 0x8771F681);
-        c = hh(c, d, a, b, block[11], 16, 0x6D9D6122);
-        b = hh(b, c, d, a, block[14], 23, 0xFDE5380C);
-        a = hh(a, b, c, d, block[1], 4, 0xA4BEEA44);
-        d = hh(d, a, b, c, block[4], 11, 0x4BDECFA9);
-        c = hh(c, d, a, b, block[7], 16, 0xF6BB4B60);
-        b = hh(b, c, d, a, block[10], 23, 0xBEBFBC70);
-        a = hh(a, b, c, d, block[13], 4, 0x289B7EC6);
-        d = hh(d, a, b, c, block[0], 11, 0xEAA127FA);
-        c = hh(c, d, a, b, block[3], 16, 0xD4EF3085);
-        b = hh(b, c, d, a, block[6], 23, 0x04881D05);
-        a = hh(a, b, c, d, block[9], 4, 0xD9D4D039);
-        d = hh(d, a, b, c, block[12], 11, 0xE6DB99E5);
-        c = hh(c, d, a, b, block[15], 16, 0x1FA27CF8);
-        b = hh(b, c, d, a, block[2], 23, 0xC4AC5665);
-
-        a = ii(a, b, c, d, block[0], 6, 0xF4292244);
-        d = ii(d, a, b, c, block[7], 10, 0x432AFF97);
-        c = ii(c, d, a, b, block[14], 15, 0xAB9423A7);
-        b = ii(b, c, d, a, block[5], 21, 0xFC93A039);
-        a = ii(a, b, c, d, block[12], 6, 0x655B59C3);
-        d = ii(d, a, b, c, block[3], 10, 0x8F0CCC92);
-        c = ii(c, d, a, b, block[10], 15, 0xFFEFF47D);
-        b = ii(b, c, d, a, block[1], 21, 0x85845DD1);
-        a = ii(a, b, c, d, block[8], 6, 0x6FA87E4F);
-        d = ii(d, a, b, c, block[15], 10, 0xFE2CE6E0);
-        c = ii(c, d, a, b, block[6], 15, 0xA3014314);
-        b = ii(b, c, d, a, block[13], 21, 0x4E0811A1);
-        a = ii(a, b, c, d, block[4], 6, 0xF7537E82);
-        d = ii(d, a, b, c, block[11], 10, 0xBD3AF235);
-        c = ii(c, d, a, b, block[2], 15, 0x2AD7D2BB);
-        b = ii(b, c, d, a, block[9], 21, 0xEB86D391);
-
-        self.state[0] = self.state[0].wrapping_add(a);
-        self.state[1] = self.state[1].wrapping_add(b);
-        self.state[2] = self.state[2].wrapping_add(c);
-        self.state[3] = self.state[3].wrapping_add(d);
-        self.count[0] = self.count[0].wrapping_add(64);
-
-        if self.count[0] < 64 {
-            self.count[1] = self.count[1].wrapping_add(1);
-        }
-    }
-}
-
-/// A simple Twofish implementation
-pub struct Twofish {
-	sbox: [u32; 256],
-	keys: [u32; 40],
-}
-
-impl Twofish {
-}
-
-/// Error types for cryptographic operations
-#[derive(Debug)]
-pub enum CryptError {
-    Align(u8, usize),
-    /// expected, got
-    Length(usize, usize), // expected, got
-}
-
-const fn f(x: u32, y: u32, z: u32) -> u32 {
-    (x & y) | (!x & z)
-}
-
-const fn g(x: u32, y: u32, z: u32) -> u32 {
-    (x & z) | (y & !z)
-}
-
-const fn h(x: u32, y: u32, z: u32) -> u32 {
-    x ^ y ^ z
-}
-
-const fn i(x: u32, y: u32, z: u32) -> u32 {
-    y ^ (x | !z)
-}
-
-const fn ff(a: u32, b: u32, c: u32, d: u32, x: u32, s: u32, ac: u32) -> u32 {
-    a.wrapping_add(f(b, c, d))
-        .wrapping_add(x)
-        .wrapping_add(ac)
-        .rotate_left(s)
-        .wrapping_add(b)
-}
-
-const fn gg(a: u32, b: u32, c: u32, d: u32, x: u32, s: u32, ac: u32) -> u32 {
-    a.wrapping_add(g(b, c, d))
-        .wrapping_add(x)
-        .wrapping_add(ac)
-        .rotate_left(s)
-        .wrapping_add(b)
-}
-
-const fn hh(a: u32, b: u32, c: u32, d: u32, x: u32, s: u32, ac: u32) -> u32 {
-    a.wrapping_add(h(b, c, d))
-        .wrapping_add(x)
-        .wrapping_add(ac)
-        .rotate_left(s)
-        .wrapping_add(b)
-}
-
-const fn ii(a: u32, b: u32, c: u32, d: u32, x: u32, s: u32, ac: u32) -> u32 {
-    a.wrapping_add(i(b, c, d))
-        .wrapping_add(x)
-        .wrapping_add(ac)
-        .rotate_left(s)
-        .wrapping_add(b)
-}
-
 /// Computes the augmented MD5 hash of the given data.
 /// If the input is empty, it returns a predefined hash value.
 /// Otherwise, it computes the MD5 hash and returns it as an array of 8 u32 values.
-pub fn augmented_md5(input: &[u8]) -> [u32; 8] {
+pub const fn augmented_md5(input: &[u8]) -> [u32; 8] {
     if input.is_empty() {
         return [
             0xEEA339DA, 0x6EBEAECA, 0xD4B6B5E, 0xBA298EBA, 0xEFBF5532, 0xC4B5A218, 0x90186095,
@@ -320,21 +63,19 @@ pub fn augmented_md5(input: &[u8]) -> [u32; 8] {
 }
 
 /// Computes the CRC32 checksum of the given data
-pub fn crc32(input: &[u8], seed: u32) -> u32 {
-    let mut crc = seed;
-
+pub const fn crc32(input: &[u8], mut seed: u32) -> u32 {
     input.iter().for_each(|b| {
-        crc = crc.wrapping_shl(8) ^ CRCTAB[(crc.wrapping_shr(24) ^ (*b as u32)) as usize]
+        seed = seed.wrapping_shl(8) ^ CRCTAB[(seed.wrapping_shr(24) ^ (*b as u32)) as usize]
     });
 
-    crc
+    seed
 }
 
 /// Encrypts or decrypts data using a simple algorithm
 /// The data must be aligned to 4 bytes.
 /// The function returns a Result containing the encrypted or decrypted data.
 /// If the data length is not a multiple of 4, an error is returned.
-pub fn data_crypt(input: &[u8], seed: u32, mul: u32, add: u32) -> Result<Vec<u8>, CryptError> {
+pub fn data_crypt(input: &[u8], mut seed: u32, mul: u32, add: u32) -> Result<Vec<u8>, CryptError> {
     if input.len() & 3 != 0 {
         return Err(CryptError::Align(4, input.len() & 3));
     }
@@ -342,7 +83,6 @@ pub fn data_crypt(input: &[u8], seed: u32, mul: u32, add: u32) -> Result<Vec<u8>
     let mut output = vec![0u8; input.len()];
     let out_blocks: &mut [u32] = cast_slice_mut(&mut output[..]);
     let in_blocks: &[u32] = cast_slice(input);
-    let mut seed = seed;
 
     in_blocks
         .iter()
@@ -359,7 +99,7 @@ pub fn data_crypt(input: &[u8], seed: u32, mul: u32, add: u32) -> Result<Vec<u8>
 /// The block must be 16 bytes long.
 /// The function returns a Result containing the encrypted or decrypted block.
 /// If the block length is not 16, an error is returned.
-pub fn file_xfer_crypt(block: &[u8], decrypt: bool) -> Result<Vec<u8>, CryptError> {
+pub const fn file_xfer_crypt(block: &[u8], decrypt: bool) -> Result<Vec<u8>, CryptError> {
     if block.len() != 16 {
         return Err(CryptError::Length(16, block.len()));
     }
@@ -402,9 +142,7 @@ pub fn file_xfer_crypt(block: &[u8], decrypt: bool) -> Result<Vec<u8>, CryptErro
 }
 
 /// Computes the FNV-1a hash of the given data
-pub fn fnv1a(input: &[u8], seed: u32) -> u32 {
-    let mut sum = seed;
-
+pub const fn fnv1a(input: &[u8], mut seed: u32) -> u32 {
     input
         .iter()
         .for_each(|b| sum = sum.wrapping_mul(0x1000193) ^ (*b as u32));
@@ -416,14 +154,13 @@ pub fn fnv1a(input: &[u8], seed: u32) -> u32 {
 /// The key is a 32-bit integer, and the data must be aligned to 4 bytes.
 /// The function returns a Result containing the encrypted or decrypted data.
 /// If the data length is not a multiple of 4, an error is returned.
-pub fn tcp_packet_crypt(key: u32, data: &[u8]) -> Result<Vec<u8>, CryptError> {
+pub const fn tcp_packet_crypt(mut key: u32, data: &[u8]) -> Result<Vec<u8>, CryptError> {
     if data.len() & 3 != 0 {
         return Err(CryptError::Align(4, data.len() & 3));
     }
 
     let mut output = vec![0u8; data.len()];
     let out_blocks: &mut [u32] = cast_slice_mut(&mut output[..]);
-    let mut key = key;
     let in_blocks: &[u32] = cast_slice(data);
 
     in_blocks
--- haxial/src/lib.rs
+++ haxial/src/lib.rs
@@ -3,6 +3,21 @@ use cfg_if::cfg_if;
 pub mod algo;
 pub use algo::*;
 
+pub mod md5;
+pub use md5::*;
+
+pub mod twofish;
+pub use twofish::*;
+
+/// Error types for cryptographic operations
+#[derive(Debug)]
+pub enum CryptError {
+    /// expected, got
+    Align(u8, usize),
+    /// expected, got
+    Length(usize, usize),
+}
+
 cfg_if! {
     if #[cfg(feature = "net")] {
         pub mod net;
